d0157d6395823d147b8e4ad08c31d4e70f311fd7
nate Dowdle <nathan.t.dowdle@gmail.com>
2022-02-10 16:24:24 -0700

    commit question 10

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -1,90 +1,90 @@
-// For problems 1-2 use the following lines of code:
-// var arr = [10,20,30,40,50,60]
+// // For problems 1-2 use the following lines of code:
+// // var arr = [10,20,30,40,50,60]
 
-// ////////// PROBLEM 1 //////////
+// // ////////// PROBLEM 1 //////////
+// // /*
+// //   Create a variable named 'firstItem' and set it equal to the first 
+// //   value of the 'arr' array.
+// // */
+// let firstItem = arr[0]
+
+// console.log(firstItem)
+// // //Code Here
+
+
+// ////////// PROBLEM 2 //////////
 // /*
-//   Create a variable named 'firstItem' and set it equal to the first 
-//   value of the 'arr' array.
+//   Remove the last item from the 'arr' array and store it in a variable
+//   named 'lastItemRemoved'. Console.log the array to verify you no 
+//   longer have the last item.
 // */
-let firstItem = arr[0]
 
-console.log(firstItem)
+// //Code Here
+// let lastItemRemoved = arr.pop() 
+
+// console.log(arr)
+
+// ////////// PROBLEM 3 //////////
+
+// // Do not edit the code below.
+// // var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
+// // Do not edit the code above.
+
+// /*
+//   Loop through the 'family' array and console.log every item.
+// */
+// for(i = 0; i < family.length; i++) {
+//   console.log(family[i])
+// }
 // //Code Here
 
 
-////////// PROBLEM 2 //////////
-/*
-  Remove the last item from the 'arr' array and store it in a variable
-  named 'lastItemRemoved'. Console.log the array to verify you no 
-  longer have the last item.
-*/
 
-//Code Here
-let lastItemRemoved = arr.pop() 
+// ////////// PROBLEM 4 //////////
 
-console.log(arr)
+// // Do not edit the code below.
+// var nums = [1,2,3,6,22,98,45,23,22,12];
+// let evensArr = []
+// // Do not edit the code above.
+// for(i = 0; i < nums.length; i++){
+//    if(nums[i] % 2 === 0){
+//     evensArr.push(nums[i])
+//   }
+// }
+// // console.log(evensArr)
 
-////////// PROBLEM 3 //////////
+// /*
+//   Use a for-loop to iterate through each number in the 'nums' array, 
+//   if the number is even, append it to the 'evensArr'.
+// */
 
-// Do not edit the code below.
-// var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
-// Do not edit the code above.
-
-/*
-  Loop through the 'family' array and console.log every item.
-*/
-for(i = 0; i < family.length; i++) {
-  console.log(family[i])
-}
-//Code Here
+// //Code Here
 
 
 
-////////// PROBLEM 4 //////////
+// ////////// PROBLEM 5 //////////
 
-// Do not edit the code below.
-var nums = [1,2,3,6,22,98,45,23,22,12];
-let evensArr = []
-// Do not edit the code above.
-for(i = 0; i < nums.length; i++){
-   if(nums[i] % 2 === 0){
-    evensArr.push(nums[i])
-  }
-}
-// console.log(evensArr)
+// // Do not edit the code below.
+// var score = 74
+// // Do not edit the code above.
 
-/*
-  Use a for-loop to iterate through each number in the 'nums' array, 
-  if the number is even, append it to the 'evensArr'.
-*/
-
-//Code Here
-
-
-
-////////// PROBLEM 5 //////////
-
-// Do not edit the code below.
-var score = 74
-// Do not edit the code above.
-
-/*
-  Determine if the letter grade of the given variable 'score'. 
-  If the variable is a 90 or above, console log an 'A', 
-  between 80 and 89, console log a 'B', between 70 and 79, 'C', 
-  between 60 and 69, 'D', and anything below 60 should console log 
-  an 'F'.
-*/
-if(score >= 90){
-  console.log("A")
-} else if(score >= 80){
-  console.log("B")
-} else if(score >= 70){
-  console.log("C")
-} else if(score >= 60){
-  console.log("D")
-} else if(score < 60)
-  console.log("F")
+// /*
+//   Determine if the letter grade of the given variable 'score'. 
+//   If the variable is a 90 or above, console log an 'A', 
+//   between 80 and 89, console log a 'B', between 70 and 79, 'C', 
+//   between 60 and 69, 'D', and anything below 60 should console log 
+//   an 'F'.
+// */
+// if(score >= 90){
+//   console.log("A")
+// } else if(score >= 80){
+//   console.log("B")
+// } else if(score >= 70){
+//   console.log("C")
+// } else if(score >= 60){
+//   console.log("D")
+// } else if(score < 60)
+//   console.log("F")
 
 
 //Code Here
@@ -104,9 +104,9 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
-let someNum = myFavoriteNumbers[4]
+// let someNum = myFavoriteNumbers[4]
 
-console.log(someNum)
+// console.log(someNum)
 
 ////////// PROBLEM 7 //////////
 
@@ -121,10 +121,10 @@ console.log(someNum)
 //exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
-if(myFavoriteNumbers.length < 7) {
-  console.log("not enough elements")
-}else(myFavoriteNumbers.length >= 7) 
-  someNum = myFavoriteNumbers[6]
+// if(myFavoriteNumbers.length < 7) {
+//   console.log("not enough elements")
+// }else(myFavoriteNumbers.length >= 7) 
+//   someNum = myFavoriteNumbers[6]
 
 
 ////////// PROBLEM 8 //////////
@@ -138,11 +138,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 //console.log '{number} is divisible by 3.'
 
 //Code Here
-for (i = 0; i < listOfNumbers.length; i++){
-  if(listOfNumbers[i] % 3 ===0){
-    console.log(`${listOfNumbers[i]} is divisable by 3`)
-  }
-}
+// for (i = 0; i < listOfNumbers.length; i++){
+//   if(listOfNumbers[i] % 3 ===0){
+//     console.log(`${listOfNumbers[i]} is divisable by 3`)
+//   }
+// }
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -153,9 +153,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the 'letters' array. 
   Console log every item in the array.
 */
-for(i = 5; i >= 0; i--){
-  console.log(letters[i])
-}
+// for(i = 5; i >= 0; i--){
+//   console.log(letters[i])
+// }
 //Code Here
 
 
@@ -181,12 +181,29 @@ If the letter grade is not one of the above letters, console.log
 */
 
 //Code Here
-
+switch(letterGrade) {
+  case 'A': console.log("The student is doing excellently.")
+  break
+  case 'B': console.log("The student is doing well.")
+  break
+  case 'C': console.log("The student is doing alright.")
+  break
+  case 'D': console.log("The student is not doing very well")
+  break
+  case 'F': console.log("The student is failing.")
+  break
+  default: console.log("not an eligible grade.")
+}
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. In this for loop, 
+  using some conditional logic, if the number your for loop is currently 
+  on is divisible by 3, console.log 'Dev'. If the number is divisible by 
+  5, console.log 'mountain'. If the number is divisible by 5 & 3, 
+  console.log 'Devmountain'. If the number is not divisible by 5 or 3, 
+  console.log the number itself. Hint: Look up the modulo operator.
 
   Your output should look like:
   1
